27fa60ceea5ed0ac782d2dbc759f749a19bc07ab
Mono1514 <dsantytrujillom@gmail.com>
2025-02-17 07:22:27 -0500

    Add files via upload

---
+++ ejercisio 2 c++.cpp
@@ -0,0 +1,11 @@
+include <iostream>
+using namespace std;
+int main(){
+    float h;
+    cout << "ingresa el precio del articulo";
+    cin>> h;
+
+   float precio_final=h-(h*0.15);
+    cout <<precio_final;
+return 0;
+}
---
+++ ejercisio 3.cpp
@@ -0,0 +1,8 @@
+include <iostream>
+using namespace std;
+int main(){
+    int a=10;
+int resultado=((((a+5)-3)*2)/4);
+cout<< resultado;
+return 0
+}
